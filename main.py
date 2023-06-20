deff520b2e6fc4f33edd4b68a3fce4883c4a76bf
arya2910 <arya291029@gmail.com>
2023-06-20 16:27:59 +0530

    arya created endpoint

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
+df = pd.read_csv("recipedata.csv")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.post("/recipe")
-async def get_recipe(request: Request):
-
-# Write your post function here -
-    return {"Data"}
+@app.post("/recipe/{recipe_id}")
+def get_recipe(recipe_id: int, servings: int):
+    recipe = df.iloc[recipe_id - 1]  # Access the row by recipe_id
+    recipe_data = {
+        "RecipeID": int(recipe['RecipeID']),
+        "Name": recipe['Name'],
+        "imageURL": "https://v5.airtableusercontent.com/v1/18/18/1687168800000/4wJVgFTbwCCDZ7BGgx8EvA/_YLzI_KX6Mgt7DKoHqjeWIIBwxM3K78JRJQtok7wmXrFEDOjAti7OxuWJ2se7oKzept4arxev2peHYPt4aPcAPZqCTnVcUuc3_Yy65z6ESjbA7POgzfQ-RsrHBFpy-jzD0t89ti89D-sJniA_jjclE5rgCEeX9wuUajKWnBg3ffG7tsxdavvVEQbXEKpYG3zCyiUxySBDHKLiIeIR52BuA/_0gBU9fD7dKSEYmEwEU2ePLsu9thGFIX2ZWU88ejUNQ",
+        "themeColor": "null",
+        "RecordID": "rec504vwu2cAuhahw",
+        "creatorID": "Dinnerfy",
+        "Recipe_Meta": recipe['Recipe_Meta'],
+        "Interest_Tags": recipe['Interest_Tags'],
+        "Diet_Tags": recipe['Diet_Tags'],
+        "Cooking_Meta": recipe['Cooking_Meta'],
+        f"Serves_{servings}": recipe[f"Serves_{servings}"]
+    }
+    return recipe_data

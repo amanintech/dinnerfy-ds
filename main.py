f5ae56897929487db1ad60354595a5f86dc8329c
Mohammad Arshad <mdarshad1000@gmail.com>
2023-06-22 00:31:17 +0530

    Add POST request feature

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import pandas as pd
 
 app = FastAPI()
 
+
+df = pd.read_csv("./recipedata.csv")
 
 @app.get("/")
 async def root():
@@ -13,8 +16,17 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-@app.post("/recipe")
-async def get_recipe(request: Request):
+@app.post("/recipe/{recipe_id}/{no_of_serve}")
+async def get_recipe(recipe_id: int, no_of_serve: int):
 
-# Write your post function here -
-    return {"Data"}
+    if recipe_id not in df["RecipeID"].values:
+        raise HTTPException(status_code=404, detail="Recipe for the given RecipeID not found")
+    
+    if f"Serves_{no_of_serve}" not in df.columns:
+        raise HTTPException(status_code=404, detail="Recipe for the given number of serve not found")
+    
+    recipe = df[["RecipeID", "Name", "Recipe_Meta", "Interest_Tags", "Diet_Tags", "Cooking_Meta", f"Serves_{no_of_serve}"]]
+    recipe = recipe[recipe['RecipeID'] == recipe_id].to_dict('records')[0]
+
+    return recipe
+    

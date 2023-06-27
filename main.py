d2de68652a48364e6de18287bcc11031e0911198
Satyam1609 <satyam1692001@gmail.com>
2023-06-27 11:22:03 +0530

    solution

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import pandas as pd
+import json
 
 app = FastAPI()
 
@@ -14,7 +16,8 @@ async def say_hello(name: str):
 
 
 @app.post("/recipe")
-async def get_recipe(request: Request):
-
-# Write your post function here -
-    return {"Data"}
+def get_recipe(request:dict):
+    data = pd.read_csv('./recipedata.csv')
+    row = data[data['RecipeID']==request["RecipeID"]]
+    index =f'Serves_{request["Servings"]}'
+    return {"Data":json.loads(row[index][0])}

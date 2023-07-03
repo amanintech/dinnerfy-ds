9a5d5388abd3eae328b259af7c954fb4bd8b30de
Satyam1609 <satyam1692001@gmail.com>
2023-07-03 12:27:20 +0530

    added defined schema

--- main.py
+++ main.py
@@ -9,50 +9,39 @@ import json
 app = FastAPI()
 
 
+class Request(BaseModel):
+    RecipeID: int
+    Servings: int
+
 
 @app.get("/")
-
 async def root():
-
     return {"message": "Hello World"}
 
 
-
 @app.get("/hello/{name}")
-
 async def say_hello(name: str):
-
     return {"message": f"Hello {name}"}
 
 
-
 @app.post("/recipe")
-
-def get_recipe(request: dict):
-
+def get_recipe(request: Request):
     data = pd.read_csv("./recipedata.csv")
 
-    RecipeID = request["RecipeID"]
+    RecipeID = request.RecipeID
 
     if RecipeID not in data["RecipeID"].values:
-
         return {
-
             "Error": "Invalid RecipeID",
-
         }
 
     row = data[data["RecipeID"] == RecipeID]
 
-    index = f'Serves_{request["Servings"]}'
+    index = f"Serves_{request.Servings}"
 
     if index not in row:
-
         return {
-
             "Error": "Invalid Servings",
-
         }
 
     return {"Data": json.loads(row[index][0])}
-
